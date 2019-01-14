89aac54d2cdea1a30c78ae02bf2acf754eae97f8
misterponce <misterponce35@gmail.com>
2019-01-14 13:49:27 +0000

    tu puta madre

--- ProyectoSQL/copia_twitter.sql
+++ ProyectoSQL/copia_twitter.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE copia_twitter;
 CREATE TABLE information (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user varchar(35) NOT NULL,
+    user varchar(20) NOT NULL,
     msg varchar(250) NOT NULL
 );
