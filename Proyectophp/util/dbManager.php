28d4ffb93c72d9b8ade6243b45eee39613c57c3b
misterponce <misterponce35@gmail.com>
2019-01-16 08:51:36 +0000

    actualicacion

--- Proyectophp/util/dbManager.php
+++ Proyectophp/util/dbManager.php
@@ -19,5 +19,26 @@
         }
             $conn = null;
     }
+
+    function mostrar(){
+        try{
+            $servername = "localhost";
+            $username   = "root";
+            $password   = "";
+            $db         = 'copia_twitter';
+            $conn       = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "Select user, msg from information ;";
+            $conn->exec($sql);
+            return true;
+        }catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+            return false;
+        }
+            $conn = null;
+    }
+    }
     
 ?>
