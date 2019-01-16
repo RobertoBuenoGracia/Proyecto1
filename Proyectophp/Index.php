e6c542b66d4cf91b432a50cece9e26f311a530c8
misterponce <misterponce35@gmail.com>
2019-01-16 08:56:02 +0000

    ponce

---
+++ Proyectophp/Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+  <form>
+    <div class="form-group">
+      <input type="text" class="form-control" id="usr">
+      <button type="button" class="btn btn-primary">Submmit</button>
+    </div>
+</body>
+</html>
