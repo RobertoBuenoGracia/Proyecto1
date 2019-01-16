f7bf9bceabfd760b100139f2391630c0a360fa04
misterponce <misterponce35@gmail.com>
2019-01-16 09:03:58 +0000

    go

--- Proyectophp/Index.php
+++ Proyectophp/Index.php
@@ -7,6 +7,11 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Document</title>
 </head>
+<?php
+require_once("main.php");
+require_once("util/dbManager.php");
+require_once("model/User.php");  
+?>
 <body>
 <div class="container">
   <form>
