a98e77a08cab81809c2fd82156fddff917aa4014
RobertoBuenoGracia <roberto.buenog@telefonica.net>
2019-01-16 09:38:40 +0000

    Responsive

--- Front-end/index.php
+++ Front-end/index.php
@@ -6,19 +6,28 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="style/main.css">
     </head>
-    <body>  
-       
-        <h1>ICSETTERS</h1>
-            <div class="panel">
-                 <?php 
-                    if (isset($_POST['submit'])){
-                        echo '<p> - <b>'.$_POST['username'].': </b>'.$_POST['msg'].'</p>';
-                    }
-                    else{
-                        echo '<p> No messages... </p>';
-                    }
-                ?>
-            </div>
+    <body> 
+        <div class="jumbotron text-center">
+            <h1>ICSETTER</h1>
+        </div> 
+            <div class="container">
+                <div class="col-sm-2">
+
+                </div>
+                <div class="panel col-sm-8">
+                    <?php 
+                        if (isset($_POST['submit'])){
+                            echo '<p> - <b>'.$_POST['username'].': </b>'.$_POST['msg'].'</p>';
+                        }
+                        else{
+                            echo '<p> No messages... </p>';
+                        }
+                    ?>
+                </div>
+                <div class="col-sm-2">
+
+                </div>
+            </div>            
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
             <label for="username" > <b>Type your username: </b></label>
             <br>
