7fb942f6a22879ef44e69fecaf613c5ac1ec93b4
Gokuby11 <gokubyy@gmail.com>
2019-01-17 09:31:49 +0000

    New Page

--- Front-end/index.php
+++ Front-end/index.php
@@ -1,13 +1,12 @@
-1
 <!DOCTYPE html>  
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="style/main.css">
+        <link rel="stylesheet" type="text/css" href="style\main.css">
     </head>
     <body> 
-        <div class="jumbotron text-center">
+        <div class="logo jumbotron text-center">
             <h1>ICSETTER</h1>
         </div> 
             <div class="container">
@@ -27,16 +26,20 @@
                 <div class="col-sm-2">
 
                 </div>
-            </div>            
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-            <label for="username" > <b>Type your username: </b></label>
-            <br>
-            <input type="text" name="username">
-            <br>
-            <br>
-            <textarea rows="4" cols="50" placeholder="Type here your tweet" name="msg"> </textarea>
-            <br>
-            <button type="submit" name="submit"> SEND </button>
-        </form>
+            </div>   
+        <div class="container"> 
+            <div class="col-sm-12">        
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                    <label for="username" > <b>Type your username: </b></label>
+                    <br>
+                    <input type="text" name="username">
+                    <br>
+                    <br>
+                    <textarea rows="4" cols="50" placeholder="Type here your tweet" name="msg"> </textarea>
+                    <br>
+                    <button type="submit" name="submit"> SEND </button>
+                </form>
+            </div>
+        </div>
     </body>
 </html>
