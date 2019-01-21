3a4d272b32504e859816d3d81429086173d2b61a
Gokuby11 <gokubyy@gmail.com>
2019-01-21 12:41:05 +0000

    Update SQL

--- ProyectoSQL/copia_twitter.sql
+++ ProyectoSQL/copia_twitter.sql
@@ -3,10 +3,15 @@ CREATE TABLE user (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_names varchar(20) NOT NULL,
     email varchar(320) NULL,
-    passwords char(40) NOT NULL
+    passwords char(40) NOT NULL,
+    'name' varchar(50) NULL,
+    surname varchar(75) NULL,
+    DNI varchar(9) NULL
 );
 
 CREATE TABLE messages (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    msg varchar(250) NOT NULL
+    msg varchar(250) NOT NULL,
+    user_id int,
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
