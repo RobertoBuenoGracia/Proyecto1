cf3183a8bc2ade2245e91d285f86024409159cc4
misterponce <46520041+misterponce@users.noreply.github.com>
2019-01-28 08:28:13 +0000

    lo

--- ProyectoSQL/copia_twitter.sql
+++ ProyectoSQL/copia_twitter.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE copia_twitter;
-CREATE TABLE user (
+CREATE TABLE usuario (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_names varchar(20) NOT NULL,
     email varchar(320) NULL,
     passwords char(40) NOT NULL,
-    'name' varchar(50) NULL,
+    name varchar(50) NULL,
     surname varchar(75) NULL,
     DNI varchar(9) NULL
 );
@@ -13,5 +13,5 @@ CREATE TABLE messages (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     msg varchar(250) NOT NULL,
     user_id int,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES usuario(id)
 );
