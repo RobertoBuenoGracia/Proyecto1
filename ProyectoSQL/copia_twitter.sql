a4e16f86e03e05ef1ac8c7e71d69c3a4eb98e933
Gokuby11 <gokubyy@gmail.com>
2019-01-30 09:26:12 +0000

    Unique

--- ProyectoSQL/copia_twitter.sql
+++ ProyectoSQL/copia_twitter.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE copia_twitter;
 CREATE TABLE usuario (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_names varchar(20) NOT NULL,
-    email varchar(320) NULL,
+    user_names varchar(20) NOT NULL UNIQUE,
+    email varchar(320) NULL UNIQUE,
     passwords char(40) NOT NULL,
     name varchar(50) NULL,
     surname varchar(75) NULL,
-    DNI varchar(9) NULL
+    DNI varchar(9) NULL UNIQUE
 );
 
 CREATE TABLE messages (
